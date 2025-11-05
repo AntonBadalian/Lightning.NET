788c492aeb1be41e57eb90dda4572ef50e84d59d
AntonUni <diamonddiamondking@gmail.com>
2025-11-05 12:36:44 +0000

    Checked if LMDB supports NativeAOT

--- src/LightningDB.Benchmarks/Main.cs
+++ src/LightningDB.Benchmarks/Main.cs
@@ -1,11 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
 using BenchmarkDotNet.Running;
 
 namespace LightningDB.Benchmarks;
 
-public static class Entry 
+public static class Entry
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine(RuntimeInformation.FrameworkDescription);
         //BenchmarkRunner.Run<WriteBenchmarks>();
         BenchmarkRunner.Run<ReadBenchmarks>();
     }
--- src/SecondProcess/Program.cs
+++ src/SecondProcess/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using LightningDB;
 
@@ -9,18 +11,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        var name = args.First();
-        using var env = new LightningEnvironment(name);
-        env.Open(EnvironmentOpenFlags.ReadOnly);
-        byte[] results;
-        using (var tx = env.BeginTransaction(TransactionBeginFlags.ReadOnly))
+        try
+        {
+            Expression.Lambda(Expression.Constant(42)).Compile();
+            Console.WriteLine("JIT detected: dynamic compilation works");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Native AOT detected: dynamic compilation not supported");
+        }
+
+        // Use a temp folder
+        var tempPath = Path.Combine(Path.GetTempPath(), "lmdb_test");
+        Directory.CreateDirectory(tempPath);
+
+        using var env = new LightningEnvironment(tempPath);
+        env.Open(EnvironmentOpenFlags.None);
+
+        // Start a write transaction to insert test data
+        using (var tx = env.BeginTransaction())
         {
             using var db = tx.OpenDatabase();
-            var result = tx.Get(db, "hello"u8.ToArray());
-            results = result.value.CopyToNewArray();
+            tx.Put(db, Encoding.UTF8.GetBytes("hello"), Encoding.UTF8.GetBytes("world"));
             tx.Commit();
         }
 
-        Console.WriteLine(Encoding.UTF8.GetString(results));
+        // Read it back
+        using (var tx = env.BeginTransaction(TransactionBeginFlags.ReadOnly))
+        {
+            using var db = tx.OpenDatabase();
+            var result = tx.Get(db, Encoding.UTF8.GetBytes("hello"));
+            var value = result.value.CopyToNewArray();
+            Console.WriteLine(Encoding.UTF8.GetString(value)); // prints "world"
+            tx.Commit();
+        }
     }
 }
